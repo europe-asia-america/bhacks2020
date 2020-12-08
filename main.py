6892c999fece3f398ec28220b8bee6cd8b8e62cd
ChompHeaveGutsy <HairsClapsGrant@wwjmp.com>
2020-12-08 18:42:52 +0530

    Add Home and About, with templates

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
+
 app = Flask(
         __name__,
         template_folder="templates",
         static_folder="static")
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    """Landing page route."""
+    nav = [
+            {"name": "Home", "url": url_for('home')},
+            {"name": "About", "url": url_for('about')},
+            {"name": "Add", "url": "https://example.com/3"},
+            ]
+    return render_template(
+        "home.html",
+        nav=nav,
+        title="Increment",
+        description="An incremental task management application.",
+    )
+
+@app.route('/about')
+def about():
+    """About page route."""
+    nav = [
+            {"name": "Home", "url": url_for('home')},
+            {"name": "About", "url": url_for('about')},
+            {"name": "Add", "url": "https://example.com/3"},
+            ]
+    return render_template(
+        "home.html",
+        nav=nav,
+        title="About",
+        description="An incremental task management application.",
+    )
+
